856def981e3a82aa07b271908a91d7b3390a3ac7
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2024-06-25 11:54:53 -0700

    Added a trail using css and javascript

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ const cdiv = document.querySelector('.container');
 
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
+  div.addEventListener('mouseover', 
+  e => e.target.classList.add('my-color-class')
+)
+
   cdiv.appendChild(div);
 }
