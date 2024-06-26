ade2d2a466cc8937eb3e2895867b5d02ebfe9004
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2024-06-26 12:02:29 -0700

    Created a Button to delete row

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const cdiv = document.querySelector('.container');
 
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
+  div.classList.add("delete");
   div.addEventListener('mouseover', 
   e => e.target.classList.add('my-color-class')
 )
-
   cdiv.appendChild(div);
 }
+const button = document.querySelector('.btn')
+button.addEventListener('click', refreshButton);
+
+function refreshButton(){
+document.querySelectorAll(".delete").forEach(el => el.remove());
+
+}
+
